dd31332b09910183d3d8cb87d8ddc739ac4fe40e
Jack Waller <ucdwaller@gmail.com>
2018-11-28 17:20:18 -0800

    Add lat/long position function to ApiModels

--- app/src/main/java/com/jtwaller/attomdemo/network/ApiModels.kt
+++ app/src/main/java/com/jtwaller/attomdemo/network/ApiModels.kt
@@ -1,18 +1,28 @@
 package com.jtwaller.attomdemo.network
 
+import com.google.android.gms.maps.model.LatLng
 import com.google.gson.annotations.SerializedName
 
 data class AttomPropertyResponse(
         @SerializedName("property") val attomPropertyList: List<AttomProperty>)
 
 data class AttomProperty(
-        val address: AttomPropertyAddress
-)
+        val address: AttomPropertyAddress,
+        val location: AttomPropertyLocation) {
+
+    fun position(): LatLng {
+        return LatLng(location.latitude.toDouble(), location.longitude.toDouble())
+    }
+
+}
 
 data class AttomPropertyAddress(
         val country: String,
         val line1: String, // address line 1
         val line2: String, // address line 2
         val locality: String,
-        val postal1: String
-)
+        val postal1: String)
+
+data class AttomPropertyLocation(
+        val latitude: String,
+        val longitude: String)
