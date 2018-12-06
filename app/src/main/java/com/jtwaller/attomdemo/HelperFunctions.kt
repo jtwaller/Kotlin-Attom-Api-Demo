e52de0f57c1e2d71dc1fe7740b848355b4aa46d0
Jack Waller <ucdwaller@gmail.com>
2018-12-06 11:34:35 -0800

    Add func to convert miles to meters

--- app/src/main/java/com/jtwaller/attomdemo/HelperFunctions.kt
+++ app/src/main/java/com/jtwaller/attomdemo/HelperFunctions.kt
@@ -1,3 +1,6 @@
 package com.jtwaller.attomdemo
 
 fun Double.format(digits: Int) = java.lang.String.format("%.${digits}f", this)
+
+// convert miles to meters
+fun Double.milesToMeters() = this * 1609.34
